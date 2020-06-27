3866dca23e86a278225e5de3e4ced01a28cf81e0
LabMemNo003 <LabMemNo003@gmail.com>
2020-06-27 16:22:08 +0800

    Get target html body

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const process = require('process');
 
 const fse = require('fs-extra');
 const { program } = require('commander');
+const { JSDOM } = require("jsdom");
+const { resolve } = require('path');
 
 program
     .option('-i, --index <int>', 'RFC index')
@@ -13,31 +15,53 @@ program
 
 const args = {};
 args.index = program.index || 7540;
-args.txtFile = `rfc${args.index}.txt`;
-args.txtURL = `http://www.rfc-editor.org/rfc/rfc${args.index}.txt`;
+args.textFile = `rfc${args.index}.txt`;
+args.textURL = `http://www.rfc-editor.org/rfc/rfc${args.index}.txt`;
 args.htmlFile = `rfc${args.index}.html`;
 args.htmlURL = `http://www.rfc-editor.org/rfc/rfc${args.index}.html`;
+args.htmlBodyFile = `rfc${args.index}_body.html`;
 args.cacheDir = './cache/';
 
 
 (async () => {
-    let txt = cacheGet(args.cacheDir, args.txtFile, args.txtURL);
-    let hrml = cacheGet(args.cacheDir, args.htmlFile, args.htmlURL);
+    const cache = await Cache(args.cacheDir);
+
+    // Target html body
+    const html = await cache.get(args.htmlFile, args.htmlURL);
+    const dom = new JSDOM(html);
+    const body = dom.window.document.body;
+    while (body.firstChild.nodeName !== 'PRE')
+        body.removeChild(body.firstChild);
+    await cache.save(args.htmlBodyFile, body.outerHTML);
+
+    // Process RFC text
+    const text = await cache.get(args.textFile, args.textURL);
+
 })().catch(e => console.log(e));
 
-
-async function cacheGet(dir, file, url) {
-    let data;
-    let cacheFile = path.join(dir, file);
-
+async function Cache(dir) {
     await fse.ensureDir(dir);
-    if (await fse.pathExists(cacheFile)) {
-        data = await fse.readFile(cacheFile);
-    } else {
-        data = await httpGet(url);
-        await fse.writeFile(cacheFile, data);
+
+    const cache = {};
+    cache.get = async (file, url) => {
+        let data;
+        let cacheFile = path.join(dir, file);
+
+        if (await fse.pathExists(cacheFile)) {
+            data = await fse.readFile(cacheFile);
+        } else if (url !== undefined) {
+            data = await httpGet(url);
+            await fse.writeFile(cacheFile, data);
+        } else {
+            throw Error('No cached file, and URL is not given!');
+        }
+        return data;
     }
-    return data;
+    cache.save = async (file, data) => {
+        await fse.writeFile(path.join(dir, file), data);
+    }
+
+    return cache;
 }
 
 function httpGet(url) {
@@ -49,3 +73,12 @@ function httpGet(url) {
         }).on('error', reject);
     });
 };
+
+function pressEntry() {
+    return new Promise((resolve) => {
+        process.stdin.once('readable', () => {
+            while (process.stdin.read() !== null);
+            resolve();
+        });
+    });
+}
