f178959124b690c1de11932dfcb00968f384d731
LabMemNo003 <LabMemNo003@gmail.com>
2020-07-04 01:43:58 +0800

    Add debug option

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const path = require('path');
 program
     .requiredOption('-i, --input <file>', 'Input file')
     .option('-o, --output <file>', 'Input file')
+    .option('-d, --debug', 'Debug mode')
     .parse(process.argv);
 
 const args = {};
@@ -19,6 +20,7 @@ args.ext = path.extname(args.inFile);
 args.base = path.basename(args.inFile, args.ext);
 args.dftFile = path.join(args.dir, `${args.base}.html`);
 args.outFile = program.output || args.dftFile;
+args.debug = program.debug;
 
 const rfcLinkBgnSgn = 'rfcLinkBgnSgn';
 const rfcLinkEndSgn = 'rfcLinkEndSgn';
@@ -37,11 +39,16 @@ const rfcLinkEnd = '.html';
         text = text.replace(/[ \t]+$/mg, '');
     }
 
+    { // Extract references
+
+    }
+
     // Escape html special characters before insert html elements 
     text = escapeHtml(text);
 
     { // Insert RFC links
-        const tpl_a = (ind, txt) => `<a href="${rfcLinkBgnSgn}${ind}${rfcLinkEndSgn}">${txt}</a>`;
+        let tpl_a = (ind, txt) => `<a href="${rfcLinkBgnSgn}${ind}${rfcLinkEndSgn}">${txt}</a>`;
+        if (args.debug) tpl_a = tpl_debug(tpl_a, 'red');
         // Match 'Obsoletes: 0000'
         text = text.replace(/(Obsoletes: )(\d+)/, `$1${tpl_a('$2', '$2')}`);
         // Match 'RFC 0000' or 'RFCs 0000'
@@ -51,6 +58,10 @@ const rfcLinkEnd = '.html';
         text = text.replace(/(RFCs?)(\n[ \t]*)(\d+)/g, `${tpl_a('$3', '$1')}$2${tpl_a('$3', '$3')}`);
         // Match 'rfc0000'
         text = text.replace(/(rfc(\d+))/g, tpl_a('$2', '$1'));
+    }
+
+    { // Insert reference links
+
     }
 
     { // End work
@@ -71,4 +82,8 @@ function escapeHtml(unsafe) {
         .replace(/>/g, "&gt;")
         .replace(/"/g, "&quot;")
         .replace(/'/g, "&#039;");
+}
+
+function tpl_debug(tpl, color) {
+    return (...args) => `<span style="background-color:${color};">${tpl(...args)}</span>`;
 }
