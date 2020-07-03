cb3dc03465382276da0e2b901dd64ece5a35571e
LabMemNo003 <LabMemNo003@gmail.com>
2020-07-04 01:07:14 +0800

    Refactor code for extendibility

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ args.base = path.basename(args.inFile, args.ext);
 args.dftFile = path.join(args.dir, `${args.base}.html`);
 args.outFile = program.output || args.dftFile;
 
+const rfcLinkBgnSgn = 'rfcLinkBgnSgn';
+const rfcLinkEndSgn = 'rfcLinkEndSgn';
+const rfcLinkBgn = 'https://www.rfc-editor.org/rfc/rfc';
+const rfcLinkEnd = '.html';
+
 (async () => {
     let text = '' + await fse.readFile(args.inFile);
 
@@ -36,27 +41,24 @@ args.outFile = program.output || args.dftFile;
     text = escapeHtml(text);
 
     { // Insert RFC links
-        const rfcLinkBgnSgn = 'rfcLinkBgnSgn';
-        const rfcLinkEndSgn = 'rfcLinkEndSgn';
-        const rfcLinkBgn = 'https://www.rfc-editor.org/rfc/rfc';
-        const rfcLinkEnd = '.html';
+        const tpl_a = (ind, txt) => `<a href="${rfcLinkBgnSgn}${ind}${rfcLinkEndSgn}">${txt}</a>`;
         // Match 'Obsoletes: 0000'
-        text = text.replace(/(Obsoletes: )(\d+)/, `$1<a href="${rfcLinkBgnSgn}$2${rfcLinkEndSgn}">$2</a>`);
+        text = text.replace(/(Obsoletes: )(\d+)/, `$1${tpl_a('$2', '$2')}`);
         // Match 'RFC 0000' or 'RFCs 0000'
-        text = text.replace(/(RFCs? (\d+))/g, `<a href="${rfcLinkBgnSgn}$2${rfcLinkEndSgn}">$1</a>`);
+        text = text.replace(/(RFCs? (\d+))/g, tpl_a('$2', '$1'));
         // Match 'RFC\n    0000' or 'RFCs\n    0000'
         // Tip: In RFC 2616, section 3.2.1, it says 'RFCs\n   1738'
-        text = text.replace(/(RFCs?)(\n[ \t]*)(\d+)/g, `<a href="${rfcLinkBgnSgn}$3${rfcLinkEndSgn}">$1</a>$2<a href="${rfcLinkBgnSgn}$3${rfcLinkEndSgn}">$3</a>`);
+        text = text.replace(/(RFCs?)(\n[ \t]*)(\d+)/g, `${tpl_a('$3', '$1')}$2${tpl_a('$3', '$3')}`);
         // Match 'rfc0000'
-        text = text.replace(/(rfc(\d+))/g,`<a href="${rfcLinkBgnSgn}$2${rfcLinkEndSgn}">$1</a>`);
-        // End work
-        text = text.replace(rfcLinkBgnSgn,rfcLinkBgn);
-        text = text.replace(rfcLinkEndSgn,rfcLinkEnd);
+        text = text.replace(/(rfc(\d+))/g, tpl_a('$2', '$1'));
     }
 
-    {
-        text = '<pre>' + text + '</pre>';
+    { // End work
+        text = text.replace(rfcLinkBgnSgn, rfcLinkBgn);
+        text = text.replace(rfcLinkEndSgn, rfcLinkEnd);
     }
+
+    text = '<pre>' + text + '</pre>';
 
     await fse.writeFile(args.outFile, text);
     console.log('Done');
