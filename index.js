4e7ee5042cd3d57f192af0cc05fe6a08684db210
LabMemNo003 <LabMemNo003@gmail.com>
2020-07-05 19:02:58 +0800

    Add more rules

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ args.debug = program.debug;
         return res;
     })(text);
 
+    { // Insert RFC links with index of section
+        // Match 'section 1.1 of RFC 0000'
+        text = text.replace(/section (\d+(?:\.\d+)*) of RFC (\d+)/ig, (mat, sind, rind) => t.enc(t.rfc_sec, rind, sind, mat));
+        // Match 'section\n 1.1\n of\n RFC\n 0000'
+        text = text.replace(/section(?:(?:\n[ \t]*)| )(\d+(?:\.\d+)*)(?:(?:\n[ \t]*)| )of(?:(?:\n[ \t]*)| )RFC(?:(?:\n[ \t]*)| )(\d+)/ig, (mat, sind, rind) => {
+            return mat.replace(/(.*)(\n[ \t]*)(.*)/g, (_, p1, mid, p2) => t.enc(t.rfc_sec, rind, sind, p1) + mid + t.enc(t.rfc_sec, rind, sind, p2));
+        });
+    }
+
     { // Insert section and page links in Table of Contents
         // Match '   1.1  Introduction .................... 10'
         text = text.replace(/(?<=^[ \t]*)(\d+(?:\.\d+)*)(  .+\.+)(\d+)$/mg, (_, sind, mid, pind) => t.enc(t.to_sec, sind, sind) + mid + t.enc(t.to_page, pind));
@@ -61,14 +70,14 @@ args.debug = program.debug;
         // Match 'sections 1.1, 2.2, 3.3'
         text = text.replace(/(?<=sections?)(((,?( |\n *)|,?( |\n *)and( |\n *))(\d+(\.\d+)*)){2,})/ig, (_, secs) => {
             return secs.replace(/\d+(\.\d+)*/g, (_, ind) => t.enc(t.to_sec, ind, ind));
-        })
+        });
     }
 
     { // Insert RFC links
         // Match 'Obsoletes: 0000'
         text = text.replace(/(?<=Obsoletes: )(\d+)/, (_, ind) => t.enc(t.rfc, ind, ind));
         // Match 'RFC 0000' or 'RFCs 0000'
-        text = text.replace(/(RFCs? (\d+))/g, (mat, ind) => t.enc(t.rfc, ind, mat));
+        text = text.replace(/RFCs? (\d+)/g, (mat, ind) => t.enc(t.rfc, ind, mat));
         // Match 'RFC\n    0000' or 'RFCs\n    0000'
         // Tip: In RFC 2616, section 3.2.1, it says 'RFCs\n   1738'
         text = text.replace(/(RFCs?)(\n[ \t]*)(\d+)/g, (_, rfc, mid, ind) => t.enc(t.rfc, ind, rfc) + mid + t.enc(t.rfc, ind, ind));
